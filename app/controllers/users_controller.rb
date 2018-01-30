eaaa62713da331b2d12a3fa631231c17001abc60
Yuma Okabe <rozabmmsr@gmail.com>
2018-01-30 13:53:59 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #保存成功
+      flash[:success] = "Welcome to Sample App!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
